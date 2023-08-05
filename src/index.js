a5a411e10f5f053309a87aeb71f99a88cb1450bf
Wck-iipi <21dcs006@nith.ac.in>
2023-08-05 18:37:05 +0530

    Added degit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const inquirer = require("inquirer");
 const TreePrompt = require("inquirer-tree-prompt");
+const degit = require("degit");
 
 const templates = require("./templates.json");
 
 inquirer.registerPrompt("tree", TreePrompt);
 inquirer.prompt(templates).then((answers) => {
-  //answers is in JSON
+  const { template } = answers;
+  console.log("Running");
+  console.log(template);
+  const emitter = degit(template, {
+    cache: true,
+    force: true,
+    verbose: true,
+  });
+
+  emitter.on("info", (info) => {
+    console.log(info.message);
+  });
+
+  emitter.clone("temp").then(() => {
+    console.log("done");
+  });
 });
