7d92ce7b3623275e9e2f811da9658735ead32bb7
Wck-iipi <21dcs006@nith.ac.in>
2023-08-05 20:08:27 +0530

    Added prompt for location.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 const inquirer = require("inquirer");
 const TreePrompt = require("inquirer-tree-prompt");
+// const { input } = require("@inquirer/prompts")
 const degit = require("degit");
+
 
 const templates = require("./templates.json");
 
 inquirer.registerPrompt("tree", TreePrompt);
-inquirer.prompt(templates).then((answers) => {
-  const { template } = answers;
-  console.log("Running");
-  console.log(template);
-  const emitter = degit(template, {
-    cache: true,
-    force: true,
-    verbose: true,
-  });
+inquirer.prompt(templates).then((templateObject) => {
+  inquirer.prompt(
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Enter the location of the project: ',
+      default() {
+        return './'
+      }
+    }
+  ).then((locationObject) => {
+    const { template } = templateObject;
+    const { location } = locationObject;
+    const emitter = degit(template, {
+      cache: false,
+      force: true,
+      verbose: true,
+    });
 
-  emitter.on("info", (info) => {
-    console.log(info.message);
-  });
+    emitter.on("info", (info) => {
+      console.log(info.message);
+    });
 
-  emitter.clone("temp").then(() => {
-    console.log("done");
-  });
+    emitter.clone(location).then(() => {
+      console.log("done");
+    });
+  })
 });
