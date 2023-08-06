a68bc7242dfb438a8952efed563bb0ec1838ec61
Wck-iipi <21dcs006@nith.ac.in>
2023-08-06 20:12:02 +0530

    Added functionality of adding new template.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,101 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const TreePrompt = require("inquirer-tree-prompt");
-// const { input } = require("@inquirer/prompts")
 const degit = require("degit");
-
 
 const templates = require("./templates.json");
 
-inquirer.registerPrompt("tree", TreePrompt);
-inquirer.prompt(templates).then((templateObject) => {
-  inquirer.prompt(
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Enter the location of the project: ',
-      default() {
-        return './'
+function writeJSONFile(filename, jsonData) {
+  const jsonString = JSON.stringify(jsonData);
+  fs.writeFileSync(filename, jsonString);
+}
+
+function searchAndInsert(data, valueToBeFound, name, value) {
+  if (Array.isArray(data)) {
+    for (const item of data) {
+      if (item.value === valueToBeFound) {
+        if (!item.children) {
+          item.children = [];
+        }
+        item.children.push({ name, value });
+        return true;
+      }
+
+      if (
+        item.children &&
+        searchAndInsert(item.children, valueToBeFound, name, value)
+      ) {
+        return true;
       }
     }
-  ).then((locationObject) => {
-    const { template } = templateObject;
-    const { location } = locationObject;
-    const emitter = degit(template, {
-      cache: false,
-      force: true,
-      verbose: true,
-    });
+  }
 
-    emitter.on("info", (info) => {
-      console.log(info.message);
-    });
+  return false;
+}
 
-    emitter.clone(location).then(() => {
-      console.log("done");
-    });
-  })
+inquirer.registerPrompt("tree", TreePrompt);
+inquirer.prompt(templates).then((templateObject) => {
+  const { template } = templateObject;
+  if (template === "newTemplate") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "templateName",
+          message: "Enter the template name: ",
+        },
+        {
+          type: "input",
+          name: "templateURL",
+          message: "Enter the template URL: ",
+        },
+      ])
+      .then((templateToBeAddedObject) => {
+        inquirer
+          .prompt(templates)
+          .then((nameWhereTemplateToBeInsertedObject) => {
+            const { templateName, templateURL } = templateToBeAddedObject;
+            const { template } = nameWhereTemplateToBeInsertedObject;
+
+            if (
+              searchAndInsert(
+                templates.tree,
+                template,
+                templateName,
+                templateURL
+              )
+            ) {
+              writeJSONFile(path.join(__dirname + "templates.json"), templates);
+            } else {
+              console.log("Name not found in templates.json.");
+            }
+          });
+      });
+  } else {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "location",
+        message: "Enter the location of the project: ",
+        default() {
+          return "./";
+        },
+      })
+      .then((locationObject) => {
+        const { location } = locationObject;
+        const emitter = degit(template, {
+          cache: false,
+          force: true,
+          verbose: true,
+        });
+
+        emitter.on("info", (info) => {
+          console.log(info.message);
+        });
+
+        emitter.clone(location).then(() => {
+          console.log("done");
+        });
+      });
+  }
 });
